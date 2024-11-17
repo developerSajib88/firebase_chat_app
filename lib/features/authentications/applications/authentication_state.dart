054e04f0e6e815ca2c98170b375e386dfa0fbcd8
developerSajib88 <developersajib88@gmail.com>
2024-11-17 13:07:34 +0600

    Authentication state create.

---
+++ lib/features/authentications/applications/authentication_state.dart
@@ -0,0 +1,46 @@
+import 'package:equatable/equatable.dart';
+import 'package:feature_first/data/model/user/user.dart';
+import 'package:flutter/cupertino.dart';
+
+class AuthenticationState extends Equatable{
+  final bool? isLoading;
+  final UserModel? userModel;
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  const AuthenticationState({
+    required this.isLoading,
+    required this.userModel,
+    required this.nameController,
+    required this.emailController,
+    required this.passwordController
+  });
+
+  factory AuthenticationState.init()=> AuthenticationState(
+    isLoading: false,
+    userModel: null,
+    nameController: TextEditingController(),
+    emailController: TextEditingController(),
+    passwordController: TextEditingController()
+  );
+
+  @override
+  List<Object?> get props => [
+    isLoading
+  ];
+
+  AuthenticationState copyWith({
+    bool? isLoading,
+    UserModel? userModel,
+    TextEditingController? nameController,
+    TextEditingController? emailController,
+    TextEditingController? passwordController
+  })=> AuthenticationState(
+    isLoading: isLoading ?? this.isLoading,
+    userModel: userModel ?? this.userModel,
+    nameController: nameController ?? this.nameController,
+    emailController: emailController ?? this.emailController,
+    passwordController: passwordController ?? this.passwordController
+  );
+
+}
