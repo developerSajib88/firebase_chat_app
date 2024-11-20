d769973d5154ce33fdbeed0e7d743c4d0da8a42a
developerSajib88 <developersajib88@gmail.com>
2024-11-20 23:53:45 +0600

    Active profile widget state ref create.

--- lib/features/dashboard/widgets/active_profile_widget.dart
+++ lib/features/dashboard/widgets/active_profile_widget.dart
@@ -1,15 +1,23 @@
 import 'package:feature_first/common/widgets/profile_widget.dart';
+import 'package:feature_first/core/dependency_injection/dependency_injection.dart';
 import 'package:feature_first/features/chat/presentation/chat_screen.dart';
 import 'package:feature_first/utils/constants/ui_constants.dart';
 import 'package:feature_first/utils/styles/custom_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ActiveProfileWidget extends StatelessWidget {
-  const ActiveProfileWidget({super.key});
+class ActiveProfileWidget extends HookConsumerWidget {
+  const ActiveProfileWidget({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final dashboardState = ref.watch(dashboardProvider);
+    final dashboardCtrl = ref.watch(dashboardProvider.notifier);
+
     return SizedBox(
       height: 60.h,
       child: ListView.builder(
@@ -27,7 +35,7 @@ class ActiveProfileWidget extends StatelessWidget {
                   gap4,
               
                   Text(
-                    "Perez",
+                    dashboardState.users?[index].fullName ?? "",
                     style: CustomTextStyles.secondary.copyWith(
                       fontWeight: FontWeight.normal
                     ),
